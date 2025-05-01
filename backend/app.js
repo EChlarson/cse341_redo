0df573beef92f34f471da657621f5877bf0d58b0
EChlarson <chl20001@byui.edu>
2025-04-30 22:30:46 -0500

    data visible on live server

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const professionalRoutes = require('./routes/professional');
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Your mock data goes here' });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/professional', professionalRoutes);
+
+app.listen(8080);
