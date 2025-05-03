1509573b374d63f1446783081588f344852a026b
EChlarson <chl20001@byui.edu>
2025-05-02 21:42:50 -0500

    fixed landing page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,9 @@ const router = express.Router();
 //This tells Express: For any route that starts with /contacts, use the routing logic defined in routes/contacts.js.
 router.use('/contacts', require('./contacts'))
 
+routes.get('/', (req, res) => {
+   res.send('Lesson 2 Landing Page!');
+});
+
 //This exports the router so it can be used in app.js.
 module.exports = router;
